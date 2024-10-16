57a542cf3ce001a5b6736381c2181178a08155a2
viys <2628587386@qq.com>
2024-10-17 00:47:06 +0800

    fix

--- animal.c
+++ animal.c
@@ -3,25 +3,25 @@
 #include "stdio.h"
 #include "string.h"
 
-void animal_init(void* t, Animal_Attribute* attr) {
+static void animal_init(void* t, Animal_Attribute* attr) {
     ANIMAL* this = (ANIMAL*)t;
 
     memcpy(&this->attr, attr, sizeof(Animal_Attribute));
 }
 
-void animal_show_sound(void* t) {
+static void animal_show_sound(void* t) {
     ANIMAL* this = (ANIMAL*)t;
 
     printf("Sound: %s\n", this->attr.sound);
 }
 
-void animal_get_name(void* t) {
+static void animal_get_name(void* t) {
     ANIMAL* this = (ANIMAL*)t;
 
     printf("Name: %s\n", this->attr.name);
 }
 
-void animal_get_age(void* t) {
+static void animal_get_age(void* t) {
     ANIMAL* this = (ANIMAL*)t;
 
     printf("Age: %d\n", (int)this->attr.age);
